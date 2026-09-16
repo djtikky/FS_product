940ee84b7b7eeb8f131af25f6b26cb7467484516
djtikky <42679697+djtikky@users.noreply.github.com>
2026-09-16 21:41:27 +0700

    fix server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,10 +11,8 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 
-const server = app.listen(PORT, () => {
-
-  console.log(`API running at http://localhost:${PORT}`);
-
+const server = app.listen(PORT, '0.0.0.0', () => {
+  console.log(`API running at port ${PORT}`);
 });
 
 
