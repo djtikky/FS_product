c7055184b6b3e726e4e442359deaabe743f31a69
djtikky <42679697+djtikky@users.noreply.github.com>
2026-09-17 17:27:23 +0700

    fix pool.js

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -1,29 +1,19 @@
 import pg from "pg";
-
 import dotenv from "dotenv";
-
 
 const environment = process.env.NODE_ENV || "development";
 
-
 if (environment !== "production") {
-
   dotenv.config({
-
     path: environment === "test" ? ".env.test" : ".env"
-
   });
-
 }
-
 
 const { Pool } = pg;
 
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
-
 
 export default pool;
